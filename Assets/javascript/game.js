529c46810512d0c21fda1de322c0d1da4acdbc0e
Ebony Meadows <ebony.meadows@gmail.com>
2019-08-17 20:55:45 -0700

    updated game again

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -3,7 +3,7 @@
 var musicObject = {
     bass: "bass",
     house: "house",
-    trap: "trap"   
+    trap: "trap"
 };
 
 //global variables
@@ -16,37 +16,46 @@ console.log(lettersWrong);
 //function
 //the next line of code: adds an _ for each letter in the word being guested.
 for (i = 0; i < musicObject.bass.length; i++) {
-    console.log ("added an underscore");
-    document.getElementById ("bass").innerHTML +=
-    "<span>&nbsp;_&nbsp</span>";
+    console.log("added an underscore");
+    document.getElementById("bass").innerHTML +=
+        "<span>&nbsp;_&nbsp</span>";
 }
 
 for (i = 0; i < musicObject.house.length; i++) {
-    console.log ("added an underscore");
-    document.getElementById ("house").innerHTML +=
-    "<span>&nbsp;_&nbsp</span>";
+    console.log("added an underscore");
+    document.getElementById("house").innerHTML +=
+        "<span>&nbsp;_&nbsp</span>";
 }
 
 for (i = 0; i < musicObject.trap.length; i++) {
-    console.log ("added an underscore");
-    document.getElementById ("trap").innerHTML +=
-    "<span>&nbsp;_&nbsp</span>";
+    console.log("added an underscore");
+    document.getElementById("trap").innerHTML +=
+        "<span>&nbsp;_&nbsp</span>";
 }
 
 
 //prints the corresponding letter pressed to the html
 
-document.onkeyup = function(event){
+document.onkeyup = function (event) {
     userGuess = event.key;
     console.log(lettersWrong);
 
-if (musicObject.bass){
-
-}
-
-else if (){
-
-}
+    if (musicObject.bass.indexOf(userGuess) == -1) {
+        console.log('${userGuess} ifs not in this word.');
+        document.getElementById("lose-text").innerHTML += userGuess;
+    }
+    else if (musicObject.bass.indexOf(userGuess) >= 0) {
+        //the location of the correct letter.
+        for (i = 0; i < musicObject.bass.length; i++) {
+            if (userGuess === musicObject.bass.name[i]) {
+                //correct letter in location
+                console.log(correct letter at location ${ i });
+                document.getElementById(letter${ i }).innerHTML = userGuess;
+            } else {
+                //these are not the location
+            }
+        }
+    }
 
 
 }
